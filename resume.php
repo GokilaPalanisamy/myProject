cc5211a560efe2ac1c963a725b7c2131517d57b1
Gokila P <sarangoki005@gmail.com>
2017-08-07 10:45:56 +0530

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>myWebsite</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header class="navbar-default navbar-fixed-top bgcolor">
+	<div class="container">
+	<div class="row col-md-12 width-responsive">
+	<a href="" class="navbar-left"><img class="img-circle ping" src="images.jpg"></a>
+	<h1 class="text h1-margin"><b class="fstword">GOKILA</b><b class="secword">PALANISAMY</b></h1>
+	</div>
+	</div>
+</header>
+	<section class="img">
+     <div class="container">
+	 <div class="row col-md-12 texting">
+	    <h2 class="margin margin-screen">126A,Kamaraj Nagar,</h2>
+        <h2 class="">4th Cross Road,</h2>
+        <h2 class="">Chennai,</h2>
+        <h2 class="">638 051.</h2>
+
+		<h2 class=""><b>e-mail : </b><a class="texting" href="">sarangoki@gmail.com</a></h2>
+		<h2 class=""><b>Phone no :</b> +91 8838 452612</h2>
+		<h2 class=""><b>Guvi URL :</b> <a href="https://www.guvi.in/sarangoki005" class="texting">https://www.guvi.in/sarangoki005</a></h2>
+		<h2 class=""><b>Hackerrank URL : </b><a href="https://github.com/GokilaPalanisamy" class="texting">https://github.com/GokilaPalanisamy</a></h2>
+		<h2 class=""><b>Github URL : </b><a href="https://www.hackerrank.com/sarangoki005" class="texting">https://www.hackerrank.com/sarangoki005</a></h2>
+	</div>
+	<div class="row col-md-12 texting">
+	<h1><u><b>OBJECTIVE </b></u></h1>
+	<h1> </h1>
+	<h2 class="text-indent">       To work efficiently and effectively in the field of Computer Science 
+		                  and Engineering so as to achieve self realization and accomplishment of 
+		                  organizational goals.</h2>
+	</div>
+	<div class="texting">
+	<h1><u><b>EDUCATIONAL QUALIFICATIONS</b></u></h1>
+    <h1></h1>
+    <div class="row col-md-12 table table-responsive">
+    <table class="center-table"><tr><th>Course</th>
+    <th>Board/University</th>
+    <th>Instituition</th>
+    <th>Year of Passing</th>
+    <th>Percentage/CGPA</th></tr>
+    <tr>
+    	<th>BE</th>
+    	<td>Anna University</td>
+    	<td>Akshaya College of Engineering and Technology.</td>
+    	<td>2018</td>
+    	<td>7.30*</td>
+    </tr>
+    <tr>
+    <th>HSC</th>
+    	<td>State Board</td> 
+    	<td>Govt. Hr Sec School.</td>
+    	<td>2014</td>
+    	<td>83%</td>
+    </tr>
+    <tr><th>SSLC</th>
+    <td>State Board</td>
+    <td>Komarappa Sengunthar Hr Sec School,Chennimalai.</td>
+    <td>2012</td>
+    <td>85.2%</td></tr></table>
+    <h3>*Upto 6th Semester</h3>
+	</div></div>
+	<div class="texting">
+	<h1 class="row col-md-12"><u><b>TECHNICAL SKILL SET</b></u></h1>
+	<div class="row col-md-12"><ul class="">
+	     <li><b>Languages			: </b>C,C++,Java.</li>
+	     <li><b>Packages 			:</b> MS-Office,Rational Rose Enterprise(UML Diagrams).</li>
+	     <li><b>Database            : </b>My SQL,MS Access.</li>
+	     <li><b>Scripting Languages :</b>HTML,CSS and Bootstrap.</li></ul>
+	</div></div>
+	<div class="texting">
+		<h1 class="row col-md-12"><u><b>AREA OF SPECIALIZATION</b></u></h1>
+		<div class="row col-md-12"><ul class="">
+	     <li>Data Structures</li>
+	     <li>Web Design</li>
+	     <li>Compiler Design-YACC Compiler</li>
+	     </ul>
+	</div>
+	</div>
+	<div class="texting">
+		<h1 class="row col-md-12"><u><b>PROJECT DETAILS</b></u></h1>
+		<div class="row col-md-12 text-indent1">
+		<h2 class=""><b>Project Details: </b>E-Basket</h2>
+		<h2><b>Technology-Used:</b>HTML,CSS and Bootsrap.</h2>
+		<h2 class=""><b>Description:</b>E-Basket is an Online Shopping site designed primarily for use in purchasing the cloths and  accessary items from our website. The System also allows to quickly and easily managing an online menu which customers can browse and use to place orders with just few clicks. The ordered items by the Customers can be easily purchased by showing the received message or mail.</div>
+        </h2>
+	    
+	</div>
+	<div class="texting">
+	<h1 class="row col-md-12"><u><b>EXTRA CURRICULUR ACTIVITIES/ACHIEVEMENTS</b></u></h1>
+	<div class="row col-md-12"><ul class="">
+	     <li>Won First Prize in Word Puzzle Organized During the Annual Day Celebration <p>in Akshaya College of Engineering and Technology.</p></li>
+<li>Won Second Prize in Drawing Competition in District Level, Erode. </li></ul></div></div>
+<div class="texting">
+	<h1 class="row col-md-12"><u><b>CO-CURRICULUR ACTIVITIES/ACHIEVEMENTS</b></u></h1>
+	<div class="row col-md-12"><ul class="">
+
+
+<li>Certified C programmer conducted by Guvi.</li>
+<li>Certified JAVA programmer conducted by Guvi.</li>
+<li>Certified PYTHON programmer conducted by Guvi.</li>
+<li>Participated JAVA Training organized by Akshaya College of Engineering and <p>Technology, Coimbatore during December 2017 to March 2017.</p></li>
+<li>Attended Internship at webjacky from may 18th  to may 27th on "Responsive <p>website creation with Bootstrap".</p></li>
+</ul></div></div>
+<div class="texting">
+	<h1 class="row col-md-12"><u><b>HOBBIES</b></u></h1>
+	<div class="row col-md-12"><ul class="">
+	     <li>Paper Quilling </li>
+<li>Pencil drawing</li></ul></div></div>
+<div class="texting">
+	<h1 class="row col-md-12"><u><b>PERSONAL INFORMATION</b></u></h1>
+	<div class="row col-md-6"><ul class="">
+	     <h2>Date of Birth : 11-05-1997</h2>				 
+	     <h2>Father  Name : Mr.XXX</h2>				
+	     <h2>Mother  Name : Mrs.XXX</h2>				
+	     <h2>Gender	:Female</h2>				
+	     <h2>Marital Status	:Single</h2>			
+	     <h2>Nationality :Indian</h2>				
+	     <h2>Languages Known : English,Tamil</h2>	</ul>
+
+<a href="https://myprofilegoki.000webhostapp.com/"><button class="center-block button" type="logout">LOGOUT</button></a>
+	</div>
+	</div>
+</section>
+
+<script type="text/javascript" href="js/bootstrap.min.js"></script>
+
+</body>
+</html>
